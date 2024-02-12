0b3dcda3f96286020cb15fdb0a16d59b51868bef
Koal M. Casler <Theeoblivion@gmail.com>
2024-02-12 12:12:25 -0400

    Added walk animations to new "Jacko" player

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CharacterController2D : MonoBehaviour
 {
     [SerializeField]
-    Rigidbody2D playerRB;
+    private Rigidbody2D playerRB;
     private Vector3 moveInput;
     public int moveSpeed = 2;
+    [SerializeField]
+    private Animator playerAnim;
 
     void Awake()
     {
@@ -24,6 +26,50 @@ public class CharacterController2D : MonoBehaviour
     }
     void Move()
     {
-        playerRB.MovePosition(transform.position + moveInput.normalized * moveSpeed * Time.fixedDeltaTime);
+        if(moveInput.x == 0 && moveInput.y == 0)
+        {
+            playerAnim.SetBool("IsIdle", true);
+        }
+        else
+        {
+            playerAnim.SetBool("IsIdle", false);
+            playerRB.MovePosition(transform.position + moveInput.normalized * moveSpeed * Time.fixedDeltaTime);
+            playerAnim.SetFloat("InputX", moveInput.x);
+            playerAnim.SetFloat("InputY", moveInput.y);
+            CheckDirection();
+        }
     }
+    void CheckDirection()
+    {
+        if(moveInput.x > 0)
+        {
+            playerAnim.SetBool("IsFacingRight", true);
+            playerAnim.SetBool("IsFacingUp", false);
+            playerAnim.SetBool("IsFacingLeft", false);
+            playerAnim.SetBool("IsFacingDown", false);
+
+        }
+        if(moveInput.y > 0)
+        {
+            playerAnim.SetBool("IsFacingUp", true);
+            playerAnim.SetBool("IsFacingLeft", false);
+            playerAnim.SetBool("IsFacingDown", false);
+            playerAnim.SetBool("IsFacingRight", false);
+        }
+        if(moveInput.x < 0)
+        {
+            playerAnim.SetBool("IsFacingLeft", true);
+            playerAnim.SetBool("IsFacingDown", false);
+            playerAnim.SetBool("IsFacingRight", false);
+            playerAnim.SetBool("IsFacingUp", false);
+        }
+        if(moveInput.y < 0)
+        {
+            playerAnim.SetBool("IsFacingDown", true);
+            playerAnim.SetBool("IsFacingUp", false);
+            playerAnim.SetBool("IsFacingLeft", false);
+            playerAnim.SetBool("IsFacingRight", false);
+        }
+    }
+
 }
